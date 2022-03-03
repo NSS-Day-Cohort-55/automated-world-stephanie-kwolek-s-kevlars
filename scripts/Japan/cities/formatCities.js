5f4d033067b2d7f6f6bb7e67eabc9d2b342d4f3b
JavontaeH <hardeman.javontae@gmail.com>
2022-03-03 09:47:29 -0600

    added correct directories to data files and started html

---
+++ scripts/Japan/cities/formatCities.js
@@ -0,0 +1,9 @@
+export const formatData = (obj) => {
+  return `
+        <article class="city-data">
+            <h3>${obj.name}</h3>
+            <p>Population: ${obj.population}</p>
+            <article>${obj.description}</article>
+        </article>
+    `;
+};
