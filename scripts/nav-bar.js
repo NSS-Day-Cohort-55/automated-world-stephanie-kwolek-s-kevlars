9fa2acd4b648f5540e5bf0d0dfe414c47b940374
JavontaeH <hardeman.javontae@gmail.com>
2022-03-03 10:41:25 -0600

    styled navbar added new temp logo to check

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -3,9 +3,8 @@ export const insertNavBar = (elementId) => {
 
   elementLocation.innerHTML = `
     <ul class="nav_bar" style="list-style-type:none;" >
-            <li class="nav_country"><a href="index.html"><img src="/images/logo/Blue Travel Blog Business logo (1).gif" alt="Home"></a></li>
-            <li class="nav_country"><a href="japan.html">Japan</a></li>
-            <li class="nav_country"><a href="boilerplate.html">Boilerplate</a></li>
+            <li class="nav_country"><a href="index.html"><img src="/images/navbar-images/logo-temp.png" alt="Home"></a></li>
+            <li class="nav_country"><a href="japan.html"><img src="/images/navbar-images/JapanFlag.png" alt="Japan"></a></li>
     </ul>
     `;
 };
