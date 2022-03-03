2aead57b67ccd59d1095836b9415e7160a3f81a5
JavontaeH <hardeman.javontae@gmail.com>
2022-03-03 14:47:33 -0600

    fixed bugs and updated iceland image

--- scripts/Iceland/Cities/FormatCities.js
+++ scripts/Iceland/Cities/FormatCities.js
@@ -1,9 +1,13 @@
-export const formatData = (obj) =>{
-    return `
+export const formatData = (obj) => {
+  return `
         <article class="city-data">
             <h3>${obj.name}</h3>
             <p>Population: ${obj.population}</p>
             <article>${obj.description}</article>
         </article>
-    `
-}
+    `;
+};
+
+export const getCities = () => {
+  return notableCities;
+};
