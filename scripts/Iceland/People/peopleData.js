d8db5783b2a0dc5bf234a5830660027931213a29
Noah Winkler <noahwinkler3700@icloud.com>
2022-03-04 14:18:14 -0600

    footer and iceland pics

--- scripts/Iceland/People/peopleData.js
+++ scripts/Iceland/People/peopleData.js
@@ -4,7 +4,7 @@ const notableCitizens = [
         , occupation: "Stongman"
         , description: "Hafþór Júlíus Björnsson is an Icelandic professional strongman and actor. He is the first person to have won the Arnold Strongman Classic, Europe's Strongest Man and World's Strongest Man in the same calendar year. He played Gregor The Mountain Clegane in the HBO series Game of Thrones for five seasons."
         , website: "https://www.imdb.com/name/nm5485781/"
-        , image: ""
+        , image: "../../../images/iceland-images/thor-image.jfif"
     }
     ,
     {
@@ -12,7 +12,7 @@ const notableCitizens = [
         , occupation: "Writer"
         , description: "Halldór Kiljan Laxness was an Icelandic writer and winner of the 1955 Nobel Prize in Literature. He wrote novels, poetry, newspaper articles, essays, plays, travelogues and short stories."
         , website: "https://www.nobelprize.org/prizes/literature/1955/laxness/biographical/"
-        , image: ""
+        , image: "../../../images/iceland-images/halldor-image.jfif"
     }
     ,
     {
@@ -20,7 +20,7 @@ const notableCitizens = [
         , occupation: "Soccer Player"
         , description: "Kolbeinn Sigþórsson is an Icelandic professional footballer who plays as a striker. Currently a free agent, he most recently played for Allsvenskan club IFK Göteborg. Before being signed by Ajax for a fee of €4.5 million in the summer of 2011, Kolbeinn came through Víkingur Reykjavik's youth program."
         , website: "https://us.soccerway.com/players/kolbeinn-sigthorsson/9605/"
-        , image: ""
+        , image: "../../../images/iceland-images/kolb-image.jfif"
     }
 ]
 
---
+++ scripts/Iceland/slideshow/imageArr.js
@@ -0,0 +1,8 @@
+export const getArr = () => {
+    const imgArr = [
+      "../../../images/iceland-images/iceland-banner-one.jpg",
+      "../../../images/iceland-images/iceland-banner-2.jpg",
+    ];
+    return imgArr;
+  };
+  
