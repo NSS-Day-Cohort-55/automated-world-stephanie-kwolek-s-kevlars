0e4fd96e1fdc7d9801fbe466ed72b3d44834af70
JavontaeH <hardeman.javontae@gmail.com>
2022-03-04 13:18:45 -0600

    fixed text for cards

--- scripts/Japan/landmarks/landmarkData.js
+++ scripts/Japan/landmarks/landmarkData.js
@@ -3,7 +3,7 @@ const notableLandMarks = [
     name: "Tokyo Tower",
     year: "1958",
     description:
-      "Tokyo Tower is a communications and observation tower in the Shiba-koen district of Minato, Tokyo, Japan, built in 1958. At 332.9 meters (1,092 ft), it is the second-tallest structure in Japan. The structure is an Eiffel Tower-inspired lattice tower that is painted white and international orange to comply with air safety regulations.",
+      "Tokyo Tower is a communications and observation tower in the Shiba-koen district of Minato, Tokyo, Japan, built in 1958. At 332.9 meters (1,092 ft), it is the second-tallest structure in Japan.",
     website: "https://www.tokyotower.co.jp/en/",
     image: "/images/japan-images/Tokyo-Tower.jpg",
   },
@@ -11,7 +11,7 @@ const notableLandMarks = [
     name: "Kinkaku-ji",
     year: "1937, 1955 (reconstruction)",
     description:
-      " Kinkaku-Ji is a Zen Buddhist temple in Kyoto, Japan. It is one of the most popular buildings in Kyoto, attracting many visitors annually. It is designated as a National Special Historic Site, a National Special Landscape and is one of 17 locations making up the Historic Monuments of Ancient Kyoto which are World Heritage Sites.",
+      " Kinkaku-Ji is a Zen Buddhist temple in Kyoto, Japan. It is one of the most popular buildings in Kyoto, attracting many visitors annually.",
     website: "https://www.shokoku-ji.jp/en/kinkakuji/",
     image: "/images/japan-images/kinkaku-ji.jpg",
   },
@@ -19,7 +19,7 @@ const notableLandMarks = [
     name: "Mount Fuji",
     year: "100,000+ years old (estimated)",
     description:
-      "Mount Fuji located on the island of Honshū, is the highest mountain in Japan, standing 3,776.24 m (12,389.2 ft). It is the second-highest volcano located on an island in Asia (after Mount Kerinci on the island of Sumatra), and seventh-highest peak of an island on Earth. Mount Fuji is an active stratovolcano that last erupted from 1707 to 1708. The mountain is located about 100 km (62 mi) southwest of Tokyo and is visible from there on clear days. Mount Fuji's exceptionally symmetrical cone, which is covered in snow for about five months of the year, is commonly used as a cultural icon of Japan and it is frequently depicted in art and photography, as well as visited by sightseers and climbers.",
+      "Mount Fuji located on the island of Honshū, is the highest mountain in Japan, standing 3,776.24 m (12,389.2 ft).",
     website: "https://www.city.himeji.lg.jp/castle/index.html",
     image: "/images/japan-images/mtfuji.jpg",
   },
--- scripts/Japan/people/peopleData.js
+++ scripts/Japan/people/peopleData.js
@@ -3,7 +3,7 @@ const notableCitizens = [
     name: "Hayao Miyazaki",
     occupation: "Animation Director",
     description:
-      "Hayao Miyazaki ,born January 5th, 1941, is a Japanese animator, director, producer, screenwriter, author, and manga artist. A co-founder of Studio Ghibli, he has attained international acclaim as a masterful storyteller and creator of Japanese animated feature films, and is widely regarded as one of the most accomplished filmmakers in the history of animation.",
+      "Hayao Miyazaki , A co-founder of Studio Ghibli, has attained international acclaim as a masterful storyteller and creator of Japanese animated feature films, and is widely regarded as one of the most accomplished filmmakers in the history of animation.",
     website: "https://www.studioghibli.com.au/",
     image: "/images/japan-images/hayao-miyazaki.jpg",
   },
@@ -19,7 +19,7 @@ const notableCitizens = [
     name: "Akira Toriyama",
     occupation: "Manga Author/Artist",
     description:
-      "Akira Toriyama ,born April 5, 1955,  is a Japanese manga artist and character designer. He first achieved mainstream recognition for his highly successful manga series Dr. Slump, before going on to create Dragon Ball (his best-known work) and acting as a character designer for several popular video games such as the Dragon Quest series, Chrono Trigger, and Blue Dragon. Toriyama is regarded as one of the artists that changed the history of manga, as his works are highly influential and popular, particularly Dragon Ball, which many manga artists cite as a source of inspiration. ",
+      "Akira Toriyama first achieved mainstream recognition for his highly successful manga series Dr. Slump, before going on to create Dragon Ball (his best-known work) and acting as a character designer for several popular video games such as the Dragon Quest series and Chrono Trigger.",
     website: "N/A",
     image: "/images/japan-images/akira-toriyama.jpg",
   },
