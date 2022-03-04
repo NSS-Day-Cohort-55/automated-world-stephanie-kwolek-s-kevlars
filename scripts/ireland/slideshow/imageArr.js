8833422fc64ac8d2f7b7b222e13c7067322d6f00
larsencs <quietus77@gmail.com>
2022-03-04 14:02:15 -0600

    banner working

--- scripts/ireland/slideshow/imageArr.js
+++ scripts/ireland/slideshow/imageArr.js
@@ -2,6 +2,7 @@ export const getArr = () => {
   const imgArr = [
     "../../../images/ireland-images/ireland-one.jpg",
     "../../../images/ireland-images/ireland-two.jpg",
+    "../../../images/ireland-images/ireland-three.jpg"
   ];
   return imgArr;
 };
