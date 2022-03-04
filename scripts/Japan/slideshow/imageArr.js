959db699f849aa133b9101c18eba9f9085518992
JavontaeH <hardeman.javontae@gmail.com>
2022-03-04 11:03:46 -0600

    added another banner for testing

--- scripts/Japan/slideshow/imageArr.js
+++ scripts/Japan/slideshow/imageArr.js
@@ -1,4 +1,7 @@
 export const getArr = () => {
-  const imgArr = ["../../../images/japan-images/japan-banner.jpg"];
+  const imgArr = [
+    "../../../images/japan-images/japan-banner.jpg",
+    "../../../images/japan-images/japan-banner2.jpg",
+  ];
   return imgArr;
 };
