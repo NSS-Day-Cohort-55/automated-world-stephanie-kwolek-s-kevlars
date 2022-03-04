8b1feece0480562251e57e4031a16c73c3de7e4d
larsencs <quietus77@gmail.com>
2022-03-04 13:47:30 -0600

    added titles to japan, iceland, and france

--- scripts/Japan/cities/renderCities.js
+++ scripts/Japan/cities/renderCities.js
@@ -5,7 +5,7 @@ export const cityRender = () => {
   const contentElement = document.querySelector(".data-card-container");
   const cities = getCities();
 
-  let renderCity = `<div class="data-cards">`;
+  let renderCity = `<div class="data-cards"><h2>Top ${cities.length} cities to visit</h2>`;
 
   for (const city of cities) {
     renderCity += formatData(city);
---
+++ scripts/ireland/slideshow/imageArr.js
@@ -0,0 +1,7 @@
+export const getArr = () => {
+  const imgArr = [
+    "../../../images/ireland-images/ireland-one.jpg",
+    "../../../images/ireland-images/ireland-two.jpg",
+  ];
+  return imgArr;
+};
